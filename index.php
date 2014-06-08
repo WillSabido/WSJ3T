46fdb5629ebe2754690f1d8a74c1633e30d10d51
Will Sabido <will@dev.webisabi.co.uk>
2014-06-08 11:25:57 +0100

    added login & user divs & corrected spans in index. Menu styling amendments & added styles for login. Corrected carousel margins & text shadow effect.

--- index.php
+++ index.php
@@ -38,31 +38,35 @@ $doc->addScript('/templates/' . $this->template . '/js/main.js', 'text/javascrip
 </head>
 <body>
     <!-- main container -->
-    <div class='container'>
+    <div class="container">
         <!-- header -->
-        <div class='row'>
-        	<header class='container-fluid'>
-        		<a href="/." id="pull-left">
-        			<img src="http://webisabihosting.co.uk/images/ws_logo.png" alt="Webi Sabi - Transient beauty in imperfection">
-        		</a>
+        <div class="row">
+        	<header class="container-fluid">
+        		<div class="span8 pull-left">
+        			<a href="/." class="pull-left">
+        				<img src="http://webisabihosting.co.uk/images/ws_logo.png" alt="Webi Sabi - Transient beauty in imperfection">
+        			</a>
+        		</div>
+        		<div class="span4 pull-right">
+					<jdoc:include type="modules" name="login" style="none" class="pull-right" />
+				</div>
         	</header>
-			<jdoc:include type="modules" name="login" style="well" id="pull-right" />
     	</div>
-    	<div class='menu row'>
-    		<div class='span12'>
+    	<div class="menu row">
+    		<div class="span12">
     			<jdoc:include type="modules" name="menu" style="xhtml" />
     		</div>
     	</div>
-    	<div class='carousel row'>
+    	<div class="carousel row">
     		<div class='span12'>
     			<jdoc:include type="modules" name="top" style="none" />
 				<jdoc:include type="modules" name="carousel" style="none" />
     		</div>
     	</div>
         <!-- mid container - includes main content area and right sidebar -->
-        <div class='mid_container row-fluid'>
+        <div class="mid_container row-fluid">
             <!-- main content area -->
-            <div class='main_content_area span9'>
+            <div class="main_content_area span9">
                 <!-- This code will create the module positions for our main content area -->
 				<jdoc:include type="modules" name="mainbody" style="none" />
                 <jdoc:include type="modules" name="breadcrumbs" style="none" />
@@ -71,25 +75,26 @@ $doc->addScript('/templates/' . $this->template . '/js/main.js', 'text/javascrip
 				<jdoc:include type="component" />
             </div>
             <!-- right sidebar -->
-            <div class='right_sidebar span3'>
+            <div class="right_sidebar span3">
                 <jdoc:include type="modules" name="rhs" style="well" />
             </div>
-            <div style='clear:both;'></div>
-            <div class='button_box span12'>
+            <div style="clear:both;"></div>
+            <div class="button_box span12">
                 <!-- This code will create the module positions for our boxes area -->
                 <jdoc:include type="modules" name="buttons" style="none" />
             </div>
-            <div style='clear:both;'></div>
+            <div style="clear:both;"></div>
         </div>
         <!-- footer -->
-        <div class='footer row'>
-            <div id='100' class='span12'>
+        <div class="footer row">
+            <div id="100" class="span12">
                 <jdoc:include type="modules" name="social" style="none" />
             	<jdoc:include type="modules" name="footer" style="xhtml" />
             	<div id="bottom">
 					<div class="user1"><jdoc:include type="modules" name="user1" style="xhtml" /></div>
 					<div class="user2"><jdoc:include type="modules" name="user2" style="xhtml" /></div>
 					<div class="user3"><jdoc:include type="modules" name="user3" style="xhtml" /></div>
+					<div class="user4"><jdoc:include type="modules" name="user4" style="xhtml" /></div>
 				</div>
 
             </div>
